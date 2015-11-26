7e354328187fa0b4dd75e22226c6d98e3f379c38
Dawid Bętkowski <dwiad1110@gmail.com>
2015-11-26 17:24:55 +0100

    Something new

--- Rejest licznika V2/Program.cs
+++ Rejest licznika V2/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace Rejest_licznika_V2
 {
-    static class Program
+    static class  Program
     {
         /// <summary>
         /// The main entry point for the application.
